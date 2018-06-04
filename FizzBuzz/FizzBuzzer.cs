adf73eb1e213f4947b135c64149a953659b95117
charleswoolfork <charleswoolfork@aol.com>
2018-06-03 22:31:30 -0400

    first commit

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string Get(int num)
+        {
+            if (IsMultipleOf3(num) && IsMultipleOf5(num))
+            {
+                return "FizzBuzz";
+            }
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+            if (IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+
+            return num.ToString();
+
+            }
+           
+          
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+        private bool IsMultipleOf5(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
+        }
+    }
+
+    
+
+
+}
